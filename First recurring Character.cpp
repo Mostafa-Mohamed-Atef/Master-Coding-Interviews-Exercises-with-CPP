5b0d3f5eb18a0e97036794cd6fc0f39d334e261f
مُصْطَفى <151864798+Mostafa-Mohamed-Atef@users.noreply.github.com>
2024-09-06 18:39:23 +0300

    Update First recurring Character.cpp

--- First recurring Character.cpp
+++ First recurring Character.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <map>
 using namespace std;
-
+//o(n+m) my solution
 int counter(int arr[]) {
     map<int,int> repeated;
     for (int i=0; i<10;i++) {
@@ -20,6 +20,20 @@ int counter(int arr[]) {
     }
     return -1;
 }
+//O(n) andrei solution
+int counter(int arr[]) {
+    map<int, int> repeated;
+    for (int i = 0; i<10; i++) {
+        if (repeated.count(arr[i])) {
+            return arr[i];
+        }
+        else {
+            repeated[arr[i]] = 1;
+        }
+        
+    }
+    return -1;
+}
 int main()
 {
     int arr[10] = {2,5,2,3,4,5,2,3,4,4};
@@ -27,6 +41,3 @@ int main()
     cout << counter(arr);
     return 0;
 }
-    
-    
-    
