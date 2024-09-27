4aba31ed53ee621b22efb18bc64bc8cd5b37f896
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-09-27 15:23:25 +0300

    bfs

---
+++ Breadth First Search.cpp
@@ -0,0 +1,21 @@
+//BFS
+void breadthFirstSearch() {
+    if (root == nullptr) return;
+    vector<int> list; //the main arr 
+    vector<Node*> queue; // to keep track the level for accessing the children 
+    queue.push_back(root);
+    while (queue.size() > 0) {
+        Node* temp = queue.front(); 
+        queue.erase(queue.begin());
+        list.push_back(temp->data);
+        if(temp->left != nullptr) {
+            queue.push_back(temp->left);
+        }
+        if (temp->right != nullptr) {
+            queue.push_back(temp->right);
+        }
+    }
+    for (int i : list) {
+        cout<<i<<endl;
+    }
+}
